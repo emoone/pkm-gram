97e13817e283e9124b1d4754b8cda01756f44b20
MOON:E <waiguogaosho@hotmail.com>
2023-03-20 22:50:21 +0900

    Update PKM 프로젝트 생성

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './styles/main.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Header Area</header>
+      <div>Content Area</div>
+
+      <footer>Footer Area</footer>
+    </div>
+  );
+}
+
+export default App;
