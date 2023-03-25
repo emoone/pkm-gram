1eb7a96425050131c816468c9b680b628d703a93
kkwondev <kkwoncokr@gmail.com>
2023-03-25 18:07:40 +0900

    feed

--- src/modules/RootRoutes.tsx
+++ src/modules/RootRoutes.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import TheLayout from '../components/common/TheLayout';
+import FeedComponent from '../pages/feed/FeedComponent';
 import HomeComponent from '../pages/home/HomeComponent';
 import ProfileComponent from '../pages/profile/ProfileComponent';
 
@@ -10,6 +11,7 @@ export default function RootRoutes() {
         <Route element={<TheLayout />}>
           <Route path="/" element={<HomeComponent />} />
           <Route path="/:loginId" element={<ProfileComponent />} />
+          <Route path="/feed" element={<FeedComponent />} />
         </Route>
       </Routes>
     </BrowserRouter>
